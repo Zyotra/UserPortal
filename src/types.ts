3d870a873700d69677dc67fc8045949522370636
Ram Krishna Yadav <itsramky234@gmail.com>
2025-12-19 19:22:48 +0545

    fixed local ws url

--- src/types.ts
+++ src/types.ts
@@ -3,8 +3,8 @@ import { FiCode } from 'react-icons/fi';
 export const AUTH_API_URL=import.meta.env.VITE_BACKEND_URL || "http://localhost:5050" as string;
 export const WEB_SERVICE_DEPLOYMENT_URL=import.meta.env.VITE_WEBSERVICE_DEPLOYMENT_URL ||"http://localhost:5053" as string;
 export const UI_DEPLOYMENT_URL=import.meta.env.VITE_UI_DEPLOYMENT_URL || "http://localhost:5056" as string;
-export const WS_URL_WEBSERVICE=import.meta.env.VITE_WS_URL_WEBSERVICE || "ws://localhost:5053/deploy-logs" as string;
-export const WS_URL_UI=import.meta.env.VITE_WS_URL_UI || "ws://localhost:5056/deploy-logs" as string;
+export const WS_URL_WEBSERVICE=import.meta.env.VITE_WS_URL_WEBSERVICE || "ws://localhost:5053" as string;
+export const WS_URL_UI=import.meta.env.VITE_WS_URL_UI || "ws://localhost:5056" as string;
 export const DEPLOYMENT_MANAGER_URL=import.meta.env.VITE_DEPLOYMENT_MANAGER_URL || "http://localhost:5051" as string;
 export const Frameworks = [
   { value: 'nodejs', label: 'Node.js', icon: SiNodedotjs, color: 'text-green-500' },
